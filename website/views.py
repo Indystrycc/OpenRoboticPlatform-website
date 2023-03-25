ae44a16f443d718087867194ed42f03939001dbe
NikodemBartnik <nikodem.bartnik@gmail.com>
2023-03-25 12:02:08 +0100

    update view

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html")
