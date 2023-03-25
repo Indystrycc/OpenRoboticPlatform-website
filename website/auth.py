c9cd9268552d3d2d8d3b3a5a2e6159e53f640f31
NikodemBartnik <nikodem.bartnik@gmail.com>
2023-03-25 23:54:18 +0100

    added working base template and other views

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return render_template('login.html')
 
 @auth.route('logout')
 def logout():
--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/library')
+def library():
+    return render_template("library.html")
