4f40e3fb808a1929c14b9568ebc9eb3468121aa1
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-03-26 23:39:23 +0200

    added part model

--- website/models.py
+++ website/models.py
@@ -16,3 +16,22 @@ class User(db.Model, UserMixin):
     confirmed = db.Column(db.Boolean, default=False)
     description = db.Column(db.String(200))
     notes = db.relationship('Note')
+
+class Part(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.String(5000))
+    #category examples: plates, wheels, other, holders & adapters for: sensors, microcontrollers & SBCs, motors, cameras 
+    category = db.Column(db.Integer)
+    file_name = db.Column(db.String(20), unique=True)
+    picture = db.Column(db.String(20), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    verified = db.Column(db.Boolean, default=False)
+    featured = db.Column(db.Boolean, default=False)
+    public = db.Column(db.Boolean, default=False)
+    rejected = db.Column(db.Boolean, default=False)
+    downloads = db.Column(db.Integer, default=0)
+    
+    
+     
