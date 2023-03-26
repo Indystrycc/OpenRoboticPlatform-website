462e49a7dbb814910a289aafeb43f0eb99f271f2
NikodemBartnik <nikodem.bartnik@gmail.com>
2023-03-26 15:28:43 +0200

    login and signup progress

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
 
 @auth.route('logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        username = request.form.get('username')
+    return render_template('signup.html')
