28fb73f79255be01bc7fd3bfb37ff8e7fc8dddd7
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-01 23:49:52 +0200

    post display and search bar added

--- website/views.py
+++ website/views.py
@@ -9,7 +9,57 @@ def home():
 
 @views.route('/library')
 def library():
-    return render_template("library.html", user = current_user)
+    posts = [
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '300mm plate',
+            'views': 10,
+            'likes': 5
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '200mm plate',
+            'views': 5,
+            'likes': 0
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '150mm plate',
+            'views': 7,
+            'likes': 2
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': 'Jetson Nano holder',
+            'views': 59,
+            'likes': 20
+        },
+         {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '300mm plate',
+            'views': 10,
+            'likes': 5
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '200mm plate',
+            'views': 5,
+            'likes': 0
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '150mm plate',
+            'views': 7,
+            'likes': 2
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': 'Jetson Nano holder',
+            'views': 59,
+            'likes': 20
+        }
+    ]
+    return render_template("library.html", user = current_user, posts=posts)
 
 @views.route('/account')
 @login_required
