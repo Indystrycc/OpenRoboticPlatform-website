9d63114e13beae3a403ce10778214f6530baaf5f
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-04 00:43:48 +0200

    added loading the parts from the db and pagination

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from .models import Part, File
 from . import db
 import os
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import Pagination
 
 views = Blueprint('views', __name__)
 
@@ -45,57 +46,10 @@ def home():
 
 @views.route('/library')
 def library():
-    posts = [
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '300mm plate',
-            'views': 10,
-            'likes': 5
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '200mm plate',
-            'views': 5,
-            'likes': 0
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '150mm plate',
-            'views': 7,
-            'likes': 2
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': 'Jetson Nano holder',
-            'views': 59,
-            'likes': 20
-        },
-         {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '300mm plate',
-            'views': 10,
-            'likes': 5
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '200mm plate',
-            'views': 5,
-            'likes': 0
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': '150mm plate',
-            'views': 7,
-            'likes': 2
-        },
-        {
-            'image': 'static/assets/img/robot2.jpg',
-            'title': 'Jetson Nano holder',
-            'views': 59,
-            'likes': 20
-        }
-    ]
-    return render_template("library.html", user = current_user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    per_page = 20
+    parts = Part.query.paginate(page=page, per_page=per_page)
+    return render_template('library.html', user = current_user, parts=parts)
 
 @views.route('/account')
 @login_required
