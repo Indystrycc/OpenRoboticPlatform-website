29b2964f6e789e3c592efa0e92bd1d554a6fd626
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-03 16:04:35 +0200

    added file model

--- website/models.py
+++ website/models.py
@@ -2,11 +2,6 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,17 +10,16 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True)
     confirmed = db.Column(db.Boolean, default=False)
     description = db.Column(db.String(200))
-    notes = db.relationship('Note')
+
 
 class Part(db.Model):
     #instead of autoincrementing it shoud be a random 5 digit hex value
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     description = db.Column(db.String(5000))
+    image = db.Column(db.String(20), unique=True)
     #category examples: plates, wheels, other, holders & adapters for: sensors, microcontrollers & SBCs, motors, cameras 
     category = db.Column(db.Integer)
-    file_name = db.Column(db.String(20), unique=True)
-    picture = db.Column(db.String(20), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     verified = db.Column(db.Boolean, default=False)
@@ -34,3 +28,11 @@ class Part(db.Model):
     rejected = db.Column(db.Boolean, default=False)
     downloads = db.Column(db.Integer, default=0)
     tags = db.Column(db.String(200))
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    part_id = db.Column(db.Integer, db.ForeignKey('part.id'))
+    file_name = db.Column(db.String(100), unique=True)
+
+    part = db.relationship('Part', backref=db.backref('files', lazy=True))
