e08e3b216475ba06061da44edb840f977693c7c0
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-03 11:44:21 +0200

    new parts display on the main page and add aprt view

--- website/views.py
+++ website/views.py
@@ -5,7 +5,39 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html", user = current_user)
+    posts = [
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '300mm plate',
+            'views': 10,
+            'likes': 5
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '200mm plate',
+            'views': 5,
+            'likes': 0
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '150mm plate',
+            'views': 7,
+            'likes': 2
+        },
+        {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': 'Jetson Nano holder',
+            'views': 59,
+            'likes': 20
+        },
+         {
+            'image': 'static/assets/img/robot2.jpg',
+            'title': '300mm plate',
+            'views': 10,
+            'likes': 5
+        }
+    ]
+    return render_template("home.html", user = current_user, newest_parts = posts)
 
 @views.route('/library')
 def library():
@@ -77,3 +109,8 @@ def designRules():
 @views.route('/showcase')
 def showcase():
     return render_template("showcase.html", user = current_user)
+
+@views.route('/addpart')
+@login_required
+def addPart():
+    return render_template("addpart.html", user = current_user)
