d2db2301660fada47ab97201a90df1e1223c1666
krzysdz <krzysdz@users.noreply.github.com>
2023-06-12 02:25:57 +0200

    Fix compose behaviour/network on Linux

--- prod/gunicorn.conf.py
+++ prod/gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
 accesslog = "-"
-bind = "0.0.0.0:8000"
+bind = "unix:/run/gunicorn/gunicorn.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "main:create_app()"
