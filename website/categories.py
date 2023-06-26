d30408f37382b82c6c4e7388ece8d322424200d1
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-26 23:28:06 +0200

    add categories to db

---
+++ website/categories.py
@@ -0,0 +1,30 @@
+categories_list = [
+    # plates
+    ("Plates", None),
+    ("Rectangular", 1),
+    ("Circular", 1),
+    ("Oval", 1),
+    ("Special", 1),
+    ("Weird", 1),
+    # holders
+    ("Holders", None),
+    ("Motors", 7),
+    ("Sensors", 7),
+    ("Microcontrollers and SBCs", 7),
+    ("Cameras", 7),
+    ("Batteries", 7),
+    ("Other", 7),
+    ("Special", 7),
+    # wheels
+    ("Wheels", None),
+    ("Smooth", 15),
+    ("Normal", 15),
+    ("Terrain", 15),
+    ("Spikes", 15),
+    ("Special", 15),
+    # connection rods
+    ("Connection rods", None),
+    ("One hole", 21),
+    ("Multi holes", 21),
+    ("Special", 21),
+]
