5262a90050c7fb31c7ce47d9a7ee5195f6887103
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-06-27 00:00:23 +0200

    add comments

--- website/categories.py
+++ website/categories.py
@@ -1,10 +1,12 @@
 categories_list = [
+    # plates
     ("Plates", None),
     ("Rectangular", 1),
     ("Circular", 1),
     ("Oval", 1),
     ("Special", 1),
     ("Weird", 1),
+    # holders
     ("Holders", None),
     ("Motors", 7),
     ("Sensors", 7),
@@ -13,12 +15,14 @@ categories_list = [
     ("Batteries", 7),
     ("Other", 7),
     ("Special", 7),
+    # wheels
     ("Wheels", None),
     ("Smooth", 15),
     ("Normal", 15),
     ("Terrain", 15),
     ("Spikes", 15),
     ("Special", 15),
+    # connection rods
     ("Connection rods", None),
     ("One hole", 21),
     ("Multi holes", 21),
