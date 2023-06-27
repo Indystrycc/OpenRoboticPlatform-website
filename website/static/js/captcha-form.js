fcb159225dd862aa45cf46b794d247864b8dbcce
krzysdz <krzysdz@users.noreply.github.com>
2023-06-27 23:37:24 +0200

    Change how recaptcha is invoked
    For some reason my callback `onSubmitCaptcha` would not be called for the second time if the button was clicked, so if the user filled the form wrong and was prompted by the browser, they would be unable to submit it after correcting. Probably when the captcha expired (2 min), it would work again, so now our script handles form submissions and invokes captcha manually.

--- website/static/js/captcha-form.js
+++ website/static/js/captcha-form.js
@@ -1,6 +1,41 @@
-window.onSubmitCaptcha = () => {
-    const captchaBtn = document.querySelector(".g-recaptcha[data-callback=onSubmitCaptcha]");
-    if (!(captchaBtn instanceof HTMLElement)) return;
-    const form = document.getElementById(captchaBtn.dataset.form);
-    if (form instanceof HTMLFormElement && form.reportValidity()) form.submit();
-}
+window.addEventListener("DOMContentLoaded", () => {
+    for (const el of document.getElementsByClassName("recaptcha")) {
+        let form = el;
+        const formId = el.dataset.form;
+        if (formId) {
+            form = document.getElementById(formId);
+        } else if (el.form instanceof HTMLFormElement) {
+            form = el.form;
+        }
+        if (!(form instanceof HTMLFormElement)) continue;
+
+        const sitekey = el.dataset.sitekey;
+        const action = el.dataset.action ?? "submit";
+
+        if (!sitekey) {
+            console.warn("An element with recaptcha class, but no data-sitekey exists.");
+            continue;
+        }
+
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            if (!form.reportValidity() || !grecaptcha) return;
+
+            grecaptcha.ready(() => {
+                grecaptcha.execute(sitekey, { action })
+                    .then(token => {
+                        const responseInput = document.createElement("textarea");
+                        responseInput.classList.add("d-none", "g-recaptcha-response");
+                        responseInput.value = token;
+                        responseInput.name = "g-recaptcha-response";
+
+                        for (const res of form.getElementsByClassName("g-recaptcha-response")) {
+                            res.parentNode.removeChild(res);
+                        }
+                        form.appendChild(responseInput);
+                        form.submit();
+                    });
+            });
+        });
+    }
+});
