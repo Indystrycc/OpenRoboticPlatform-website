156fc5c372ce20535dcc9fd28cf38419f4bdb9ef
krzysdz <krzysdz@users.noreply.github.com>
2023-06-27 22:28:19 +0200

    Move client-side validation to HTML

--- website/static/js/captcha-form.js
+++ website/static/js/captcha-form.js
@@ -2,5 +2,5 @@ window.onSubmitCaptcha = () => {
     const captchaBtn = document.querySelector(".g-recaptcha[data-callback=onSubmitCaptcha]");
     if (!(captchaBtn instanceof HTMLElement)) return;
     const form = document.getElementById(captchaBtn.dataset.form);
-    if (form instanceof HTMLFormElement) form.submit();
+    if (form instanceof HTMLFormElement && form.reportValidity()) form.submit();
 }
