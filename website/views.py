0db4f87d99877f82bdf7422790527aecbeee7d52
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-07-19 13:28:34 +0200

    remove additional database query
    Co-authored-by: krzysdz <krzysdz@users.noreply.github.com>

--- website/views.py
+++ website/views.py
@@ -70,7 +70,7 @@ def library():
 
     if selected_category != -1:
         if any(category.id == selected_category for category in categories):
-            category_group = Category.query.filter_by(parent_id=selected_category)
+            category_group = next(c.subcategories for c in categories if c.id == selected_category)
             category_ids = [category.id for category in category_group]
             parts = parts.filter(Part.category.in_(category_ids))
         else:
