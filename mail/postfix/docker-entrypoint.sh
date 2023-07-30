6c74688c8cc82cb95af24e731c297ed4c7e8ffee
krzysdz <krzysdz@users.noreply.github.com>
2023-07-30 20:57:08 +0200

    Add a simple postfix container

---
+++ mail/postfix/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MAIL_HOSTNAME="${MAIL_HOSTNAME:-postfix.orp.testing}"
+MAIL_DOMAIN="${MAIL_DOMAIN:-orp.testing}"
+
+# I could disable chroot, but this works too
+cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+
+echo "$MAIL_DOMAIN" > /etc/mailname
+postconf myhostname=$MAIL_HOSTNAME
+
+exec "$@"
