a1f7517ab6aef4d813350e9a9752b923243811be
Nikodem Bartnik <39432165+NikodemBartnik@users.noreply.github.com>
2023-08-01 12:35:57 +0200

    add update and save events

--- migrations/versions/97e7bd98496d_add_stats_table.py
+++ migrations/versions/97e7bd98496d_add_stats_table.py
@@ -34,21 +34,23 @@ def upgrade():
 
     op.execute(
         """
-        CREATE EVENT saveStats
-        ON SCHEDULE EVERY 1 DAY
+        CREATE EVENT updateStats
+        ON SCHEDULE EVERY 5 MINUTE
         DO
         INSERT INTO stats (
-        total_users,
-        total_verified_users,
-        total_parts,
-        total_verified_parts,
-        total_featured_parts,
-        total_rejected_parts,
-        total_files,
-        total_views,
-        date
+            id,
+            total_users,
+            total_verified_users,
+            total_parts,
+            total_verified_parts,
+            total_featured_parts,
+            total_rejected_parts,
+            total_files,
+            total_views,
+            date
         )
         SELECT
+            1,
             COUNT(DISTINCT user.id),
             COUNT(DISTINCT CASE WHEN user.confirmed = true THEN user.id END),
             COUNT(DISTINCT part.id),
@@ -61,11 +63,56 @@ def upgrade():
         FROM
             user
             LEFT JOIN part ON user.id = part.user_id
-            LEFT JOIN file ON part.id = file.part_id;
+            LEFT JOIN file ON part.id = file.part_id
+        ON DUPLICATE KEY UPDATE
+            total_users = VALUES(total_users),
+            total_verified_users = VALUES(total_verified_users),
+            total_parts = VALUES(total_parts),
+            total_verified_parts = VALUES(total_verified_parts),
+            total_featured_parts = VALUES(total_featured_parts),
+            total_rejected_parts = VALUES(total_rejected_parts),
+            total_files = VALUES(total_files),
+            total_views = VALUES(total_views),
+            date = NOW()
+    """
+    )
+
+    op.execute(
+        """
+        CREATE EVENT saveStats
+        ON SCHEDULE EVERY 1 DAY
+        STARTS CONCAT(CURRENT_DATE(), ' 23:59:00')
+        DO
+        INSERT INTO stats (
+            total_users,
+            total_verified_users,
+            total_parts,
+            total_verified_parts,
+            total_featured_parts,
+            total_rejected_parts,
+            total_files,
+            total_views,
+            date
+        )
+        SELECT
+            total_users,
+            total_verified_users,
+            total_parts,
+            total_verified_parts,
+            total_featured_parts,
+            total_rejected_parts,
+            total_files,
+            total_views,
+            NOW()
+        FROM
+            stats
+        WHERE
+            id = 1;
         """
     )
 
 
 def downgrade():
     op.drop_table("stats")
+    op.execute("DROP EVENT updateStats")
     op.execute("DROP EVENT saveStats")
